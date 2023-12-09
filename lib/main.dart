9e3913b417894b1d2dcd889bd092514393d42774
Vangeanc3 <ismaelmartins919@gmail.com>
2023-12-09 16:51:00 -0300

    :package: feat: add package provider e configurando

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
+import 'package:asyncstate/class/async_class.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:grimorio/firebase_options.dart';
+import 'package:grimorio/repositories/user_repository.dart';
+import 'package:grimorio/screens/components/my_loading.dart';
 import 'package:grimorio/screens/dashboard.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const Grimorio());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  AsyncState.setLoaderPersonalized(
+    defaultLoaderWidget: const MyLoading(),
+  );
+
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => UserRepository(),
+    )
+  ], child: const Grimorio()));
 }
 
 class Grimorio extends StatelessWidget {
